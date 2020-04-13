a8bce5e9d66bb0ecb5b54e043cb8a6989feaa413
Henric Backman <b-d-t@msn.com>
2020-04-13 19:44:42 +0200

    add config to turn on/off frfuel compat

--- config.lua
+++ config.lua
@@ -3,5 +3,6 @@ Config.InfiniteRepairs		= false -- Should one repairkit last forever?
 Config.RepairTime			  = 15 -- In seconds, how long should a repair take?
 Config.IgnoreAbort			= true -- Remove repairkit from inventory even if user aborts repairs?
 Config.AllowMecano			= false -- Allow mechanics to use this repairkit?
+Config.FrFuelCompat     	= true  -- If repairkit should repair the fueltank or not breaks https://github.com/thers/FRFuel
 
 Config.Locale				= 'en' -- "fr" for French, "sv" for Sweden
